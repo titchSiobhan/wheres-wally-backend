9d936f36236051c3b9bfff4502a4fbc09d17c54c
Siobhan <201608148+titchSiobhan@users.noreply.github.com>
2026-05-25 17:06:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+app.post('/check', (req, res) => {
+    const {x, y} =req.body;
+    console.log("Received:", x, y);
+
+
+    const found =  x > 680 && x < 730  && y > 240 && y < 370;
+    res.json({found})
+    
+})
+
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`Hello! We're on port ${PORT}`)
+})
